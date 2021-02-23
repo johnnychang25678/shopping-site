d74b7aaf27e55676374407b34b979b2795b74f2f
Johnny Chang <zjps3407@gmail.com>
2021-02-23 18:02:09 +0800

    add products pagination

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,30 +1,46 @@
 /* eslint-disable no-console */
 const db = require('../models')
 
-const { Product } = db
-const { Cart } = db
-const PAGE_LIMIT = 3
-const PAGE_OFFSET = 0
+const { Product, Cart } = db
+const pageLimit = 9
 
 const productController = {
   getProducts: async (req, res) => {
     try {
-      // 產品
-      const products = await Product.findAndCountAll({
-        offset: PAGE_OFFSET,
-        limit: PAGE_LIMIT,
+      let offset = 0
+      if (req.query.page) {
+        offset = (req.query.page - 1) * pageLimit
+      }
+      const productFindAll = Product.findAndCountAll({
+        offset,
+        limit: pageLimit,
         raw: true,
         nest: true,
       })
-      // 購物車
-      const cart = await Cart.findByPk(req.session.cartId, {
+
+      const cartFindbyPk = Cart.findByPk(req.session.cartId, {
         include: [{ model: Product, as: 'items' }],
       })
+
+      const [products, cart] = await Promise.all([productFindAll, cartFindbyPk])
+
+      const currentPage = Number(req.query.page) || 1
+      const pageCount = Math.ceil(products.count / pageLimit) // 12/9 = 1.33 = 2
+      const totalPages = Array.from({ length: pageCount }).map(
+        (_, index) => index + 1
+      ) // [_, _] => [1, 2]
+      const prev = currentPage - 1 < 1 ? 1 : currentPage - 1
+      const next = currentPage + 1 > pageCount ? pageCount : currentPage + 1
+
       let totalPrice = 0
       if (!cart)
         return res.render('products', {
           products,
           totalPrice,
+          currentPage,
+          totalPages,
+          prev,
+          next,
         })
       totalPrice =
         cart.items.length > 0
@@ -37,6 +53,10 @@ const productController = {
         products,
         cart: cart.toJSON(),
         totalPrice,
+        currentPage,
+        totalPages,
+        prev,
+        next,
       })
     } catch (err) {
       console.log(err)

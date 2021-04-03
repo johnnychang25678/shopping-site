9ec03faeed44ec9a184eefd5b973c59eaaaf58df
Johnny Chang <zjps3407@gmail.com>
2021-04-03 17:47:41 +0800

    prep for AWS deploy

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql')
 
+if (process.env.NODE_ENV !== 'produciton') {
+  require('dotenv').config()
+}
+
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'password',
-  port: '3306',
-  database: 'shopping_cart',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 })
 
 // eslint-disable-next-line arrow-body-style
